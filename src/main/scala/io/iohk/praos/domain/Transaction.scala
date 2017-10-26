fe09627008c1e5b28183089ab19d66f5883fb79e
Javier Diaz <jdiaz@atixlabs.com>
2017-10-26 16:55:01 -0300

    Implemented initial modeling of transactions.

--- src/main/scala/io/iohk/praos/domain/Transaction.scala
+++ src/main/scala/io/iohk/praos/domain/Transaction.scala
@@ -1,5 +1,30 @@
 package io.iohk.praos.domain
 
-class Transaction {
+import io.iohk.praos.crypto.Key
 
+
+/**
+  * The transfer of a certain amount of stake from one stakeholder to another.
+  *
+  * @param senderPublicKey  The public key for the stakeholder sending the stake.
+  * @param recipientPublicKey  The public key for the stakeholder receiving the stake.
+  * @param stake  The amount of stake to be transferred.
+  */
+case class Transaction(senderPublicKey: Key, recipientPublicKey: Key, stake: Stake) {
+
+  /**
+    * Applies a transaction to an initial stake distribution, yielding a resulting stake distribution.
+    *
+    * @param stakeDistribution  Initial stake distribution.
+    * @return  Resulting stake distribution.
+    */
+  def apply(stakeDistribution: StakeDistribution): StakeDistribution = {
+    // TODO: Handle logic errors (i.e. that the transaction makes sense in the initial distribution)
+    val resultingSenderStake = stakeDistribution(senderPublicKey) - stake
+    val resultingRecipientStake = stakeDistribution(recipientPublicKey) + stake
+    val resultingStakeDistribution = stakeDistribution
+      .updated(senderPublicKey, resultingSenderStake)
+      .updated(recipientPublicKey, resultingRecipientStake)
+    resultingStakeDistribution
+  }
 }
--- src/test/scala/io/iohk/praos/domain/TransactionSpec.scala
+++ src/test/scala/io/iohk/praos/domain/TransactionSpec.scala
@@ -1,5 +1,44 @@
 package io.iohk.praos.domain
 
-class TransactionSpec {
+import io.iohk.praos.crypto.Key
+import org.scalatest.{FlatSpec, GivenWhenThen}
 
+
+/**
+  * Unit tests for class [[io.iohk.praos.domain.Transaction]].
+  */
+class TransactionSpec extends FlatSpec with GivenWhenThen {
+
+  // Stakeholder U1
+  var publicKey1: Key = akka.util.ByteString("1")
+
+  // Stakeholder U2
+  val publicKey2: Key = akka.util.ByteString("2")
+
+  // Stakeholder U3
+  val publicKey3: Key = akka.util.ByteString("3")
+
+  // A stake distribution assigning 10 to U1, 2 to U2 and 8 to U3.
+  val stakeDistribution = StakeDistribution(publicKey1 -> 10, publicKey2 -> 2, publicKey3 -> 8)
+
+  // A transaction transferring a stake of 3 from U1 to U2.
+  val transaction = Transaction(publicKey1, publicKey2, 3)
+
+  // The resulting stake distribution of applying "transaction" to "stakeDistribution".
+  val resultingStakeDistribution = transaction.apply(stakeDistribution)
+
+  "The stakeholder U1 holding a stake of 10" should "have a resulting stake of 7" in {
+    When("a transaction transferring a stake of 3 from U1 to U2 is applied")
+    assert(resultingStakeDistribution(publicKey1) == 7)
+  }
+
+  "The stakeholder U2 holding a stake of 2" should "have a resulting stake of 5" in {
+    When("a transaction transferring a stake of 3 from U1 to U2 is applied")
+    assert(resultingStakeDistribution(publicKey2) == 5)
+  }
+
+  "A well-formed transaction" should "preserve the total stake across stake distributions" in {
+    val resultingStakeDistributionTotalStake = resultingStakeDistribution.values.sum
+    assert(resultingStakeDistributionTotalStake == 20)
+  }
 }
