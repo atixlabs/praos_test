ed6a54d905e2372b4e83cbb09519c4a5e9c6a885
mirko <mirko.alic@iohk.io>
2017-10-26 12:18:29 -0300

    PO-3 - Add unit test, fix & refactor crypto PKI methods

--- src/main/scala/io/iohk/praos/crypto/package.scala
+++ src/main/scala/io/iohk/praos/crypto/package.scala
@@ -1,49 +1,51 @@
 package io.iohk.praos
 
+import akka.util.ByteString
+
 package object crypto {
-  type Key = Array[Byte]
+  type Key = ByteString
   type KeyPair = (Key, Key)
   type SecureRandom = Int
 
   // Dummy implementation to simulate a public-private key schema.
   val keyLength = 32
-  private def genKey = (s: SecureRandom, length: Int) => new Key(length) map(_ => s.toByte)
+  private def genKey = (s: SecureRandom, length: Int) => ByteString(new Array[Byte](length) map(_ => s.toByte))
 
-  private def XOR(x: Array[Byte], y: Array[Byte]): Array[Byte] =
-    (x.toList zip y.toList).map(elements => (elements._1 ^ elements._2).toByte).toArray
-
+  private def XOR(x: ByteString, y: ByteString): ByteString =
+    ByteString((x zip y).map(elements => (elements._1 ^ elements._2).toByte).toArray)
+  
   // Post: Return (publicKey, privateKey)
   def generateKeyPair(seed: SecureRandom): KeyPair = {
     (genKey(seed + 1, keyLength), genKey(seed, keyLength))
   }
 
   def getPublicKeyFromPrivateKey(privateKey: Key): Key = {
-    privateKey map(byte => (byte - 1).toByte)
+    privateKey map(byte => (byte + 1).toByte)
   }
 
   trait Cipher {
-    def encryptWith(data: Array[Byte], publicKey: Key): Array[Byte]
-    def decryptWith(encryptedData: Array[Byte], privateKey: Key): Option[Array[Byte]]
+    def encryptWith(data: ByteString, publicKey: Key): ByteString
+    def decryptWith(encryptedData: ByteString, privateKey: Key): Option[ByteString]
   }
 
   trait Signer {
     // Post: Return signedData
-    def signedWith(data: Array[Byte], privateKey: Key): Array[Byte]
+    def signedWith(data: ByteString, privateKey: Key): ByteString
     // Post: Return (publicKey, data)
-    def stripSignature(signedData: Array[Byte]): (Key, Array[Byte])
+    def stripSignature(signedData: ByteString): (Key, ByteString)
   }
 
   object CipherImpl extends Cipher {
-    def encryptWith(data: Array[Byte], publicKey: Key): Array[Byte] = XOR(data, publicKey)
+    def encryptWith(data: ByteString, publicKey: Key): ByteString = XOR(data, publicKey)
 
-    def decryptWith(encryptedData: Array[Byte], privateKey: Key): Option[Array[Byte]] =
+    def decryptWith(encryptedData: ByteString, privateKey: Key): Option[ByteString] =
       Option(XOR(encryptedData, getPublicKeyFromPrivateKey(privateKey)))
   }
 
   object SignerImpl extends Signer {
-    def signedWith(data: Array[Byte], privateKey: Key): Array[Byte] =
-      (privateKey map (byte => (byte + 1).toByte)) ++ data
+    def signedWith(data: ByteString, privateKey: Key): ByteString =
+      getPublicKeyFromPrivateKey(privateKey) ++ data
 
-    def stripSignature(signedData: Array[Byte]): (Key, Array[Byte]) = signedData.splitAt(keyLength)
+    def stripSignature(signedData: ByteString): (Key, ByteString) = signedData.splitAt(keyLength)
   }
 }
--- src/test/scala/io/iohk/praos/crypto/cryptoSpec.scala
+++ src/test/scala/io/iohk/praos/crypto/cryptoSpec.scala
@@ -1,7 +1,40 @@
 package io.iohk.praos.crypto
 
 import org.scalatest.{FlatSpec, Matchers}
+import akka.util.ByteString
+
+import scala.util.Random
 
 class cryptoSpec extends FlatSpec with Matchers {
-  // TODO: implement unit tests
+
+  trait cryptoTestSetup {
+    val(userPublicKey, userPrivateKey)  = generateKeyPair(Random.nextInt())
+    val cipher = CipherImpl
+    val signer = SignerImpl
+    def serialize(data: String): ByteString = ByteString(data)
+  }
+
+  it should "get the user public key from its private key" in new cryptoTestSetup {
+    getPublicKeyFromPrivateKey(userPrivateKey) should be (userPublicKey)
+  }
+
+  "Cipher" should "encrypt a message using user's pk and then decrypt them using user's sk correctly" in new cryptoTestSetup {
+    val message = serialize("a secret message")
+    cipher.decryptWith(cipher.encryptWith(message, userPublicKey), userPrivateKey) should be (Some(message))
+  }
+
+  "Cipher" should "NOT decrypt an encrypted message using user's pk" in new cryptoTestSetup {
+    val message = serialize("a secret message")
+    cipher.decryptWith(cipher.encryptWith(message, userPublicKey), userPublicKey) shouldNot be (Some(message))
+  }
+
+  "Signer" should "sign a message using user's sk and then strip the signed message correctly" in new cryptoTestSetup {
+    val message = serialize("I'm stakeholder Ua")
+    signer.stripSignature(signer.signedWith(message, userPrivateKey)) should be (userPublicKey, message)
+  }
+
+  "Signer" should "NOT sign a message using user's pk and then strip the signed message correctly" in new cryptoTestSetup {
+    val message = serialize("I'm stakeholder Ua")
+    signer.stripSignature(signer.signedWith(message, userPublicKey)) shouldNot be (userPublicKey, message)
+  }
 }
