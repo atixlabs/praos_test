e116b97041ffc28cfac82bca0f9dbf61cf195a9b
Javier <javierdiaz72@hotmail.com>
2017-11-02 12:14:16 -0300

    Added further explanation about the use of the  GenesisBlock class.
    For simplicity, GenesisBlocks are also used to store intermediate values when applying a sequence of blocks to a given GenesisBlock.

--- src/main/scala/io/iohk/praos/domain/GenesisBlock.scala
+++ src/main/scala/io/iohk/praos/domain/GenesisBlock.scala
@@ -10,6 +10,12 @@ import io.iohk.praos.crypto.{Seed, combineSeeds}
   * @param genesisNonce  The nonce to be used.
   */
 case class GenesisBlock(genesisDistribution: StakeDistribution, genesisNonce: Seed) {
+
+  /**
+    * @note The application of a block to a genesis block is *not* a genesis block when the applied block is not the
+    *       last block in an epoch, at least at the conceptual level. However, for the sake of simplicity,
+    *       GenesisBlocks are also used to store intermediate results when several blocks are applied in sequence.
+    */
   def applyBlock(block: Block): GenesisBlock = {
     val newGenesisDistribution = block.data.foldLeft(genesisDistribution)(
       (sd: StakeDistribution, tx: Transaction) => applyTransaction(tx, sd)
