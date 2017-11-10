15f2c396766dc212cd6e1c498402c418d6316010
Javier <javierdiaz72@hotmail.com>
2017-11-09 23:10:28 -0300

    Initial implementation.

--- src/main/scala/io/iohk/praos/ledger/Ledger.scala
+++ src/main/scala/io/iohk/praos/ledger/Ledger.scala
@@ -1,12 +1,18 @@
 package io.iohk.praos.ledger
 
-import io.iohk.praos.crypto.combineSeeds
+import io.iohk.praos.crypto.{Key, combineSeeds}
 import io.iohk.praos.domain._
 
 trait Ledger {
   def receiveChain(currentBlockchainState: BlockchainState, newChain: Blockchain): BlockchainState
 
   def slotEnd(lastSlotState: Genesis, currentBlockChainState: BlockchainState): (Genesis, BlockchainState)
+
+  /**
+    * @note It is assumed that blocks (namely, singleton chains) are broadcast instead of chains. This implies that
+    *       it cannot be verified that blocks are correctly chained.
+    */
+  def verifyChain(slotNumber: SlotNumber, newChain: Blockchain, genesisHistory: GenesisHistory): Boolean
 }
 
 case class LedgerImpl(consensusResolver: ConsensusResolver)
@@ -28,6 +34,16 @@ case class LedgerImpl(consensusResolver: ConsensusResolver)
     (newSlotState, newBestBlockchain)
   }
 
+  override def verifyChain(slotNumber: SlotNumber, newChain: Blockchain, genesisHistory: GenesisHistory): Boolean = {
+      require(newChain.size == 1, "Only singleton chains are allowed")
+      val block = newChain.head
+
+      val genesisAtSlot = genesisHistory.getGenesisAt(slotNumber)
+
+      require(genesisAtSlot.isDefined, s"No genesis associated to $slotNumber in genesis history")
+      this.verifyBlock(block, slotNumber, genesisAtSlot.get)
+  }
+
   private def applyBlockChain(newBlockchain: Blockchain, slotState: Genesis): Genesis =
     newBlockchain.foldLeft(slotState)(
       (tempSlotState: Genesis, block: Block) => applyBlock(block, tempSlotState)
@@ -40,4 +56,10 @@ case class LedgerImpl(consensusResolver: ConsensusResolver)
     val newGenesisNonce = combineSeeds(slotState.genesisNonce, block.value.nonce._1)
     Genesis(newStakeDistribution, newGenesisNonce)
   }
+
+  private def verifyBlock(block: Block, slotNumber: SlotNumber, genesis: Genesis): Boolean = {
+    verifyIsLeader() && verifyNonce()
+  }
+
+  private def verifyIsLeader(genesis: Genesis, slotNumber: SlotNumber, publicKey: Key, proof: Ver)
 }
